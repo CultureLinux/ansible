9edf25972509c8d5fcf97e95bb969e45cf841b8b
CultureLinux <cyklodev.services@gmail.com>
2024-01-24 13:55:51 +0100

    Fix build template

--- roles/proxmox-cloud-template/templates/load_daily_cloud.j2.sh
+++ roles/proxmox-cloud-template/templates/load_daily_cloud.j2.sh
@@ -1,9 +1,12 @@
 qm destroy {{item.vmid}}
-qm create {{item.vmid}} --memory 1024 --core 1 --name {{item.vm}} --net0 virtio,bridge=vmbr0 --description "{{item.vm}}"
-qm importdisk {{item.vmid}} /tmp/{{item.file}} local-lvm
-qm set {{item.vmid}} --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-{{item.vmid}}-disk-0
-qm set {{item.vmid}} --boot c --bootdisk scsi0
-qm set {{item.vmid}} --ide2 local-lvm:cloudinit
+
+qm create {{item.vmid}} --memory 1024 --core 1 --name {{item.vm}} --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci --description "{{item.vm}}"
+qm set {{item.vmid}} --scsi0 ssd-back:0,import-from=/tmp/{{item.file}}
+qm set {{item.vmid}} --boot order=scsi0
+qm set {{item.vmid}} --ide2  ssd-back:cloudinit
 qm set {{item.vmid}} --serial0 socket --vga serial0
+
 rm -fv /tmp/{{item.file}}
-rm -fv /tmp/load_daily_cloud.sh
+rm -fv /tmp/load_daily_cloud_{{item.name}}.sh
+
+
