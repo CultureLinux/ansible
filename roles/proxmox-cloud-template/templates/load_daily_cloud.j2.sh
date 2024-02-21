c345f0dd82aee85eade2937f4f2a45272f3ed437
CultureLinux <cyklodev.services@gmail.com>
2024-02-21 08:16:11 +0100

    Fix waiting

--- roles/proxmox-cloud-template/templates/load_daily_cloud.j2.sh
+++ roles/proxmox-cloud-template/templates/load_daily_cloud.j2.sh
@@ -1,7 +1,7 @@
 qm destroy {{item.vmid}}
 
 qm create {{item.vmid}} --memory 1024 --core 1 --name {{item.vm}} --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci --description "{{item.vm}}"
-qm set {{item.vmid}} --scsi0 ssd-back:0,import-from=/tmp/{{item.file}}
+qm set {{item.vmid}} --scsi0 ssd-back:0,import-from=/tmp/{{item.file}},format=qcow2
 qm set {{item.vmid}} --boot order=scsi0
 qm set {{item.vmid}} --ide2  ssd-back:cloudinit
 qm set {{item.vmid}} --serial0 socket --vga serial0
