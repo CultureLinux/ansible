15f3b8d6dc97727c9a1a940e1eb3672104bd392b
CultureLinux <cyklodev.services@gmail.com>
2024-04-12 15:04:11 +0200

    Fix template scsi > virtio

--- roles/proxmox-cloud-template/templates/load_daily_cloud.j2.sh
+++ roles/proxmox-cloud-template/templates/load_daily_cloud.j2.sh
@@ -1,8 +1,8 @@
 qm destroy {{item.vmid}}
 
 qm create {{item.vmid}} --memory 1024 --core 1 --name {{item.vm}} --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci --description "{{item.vm}}"
-qm set {{item.vmid}} --scsi0 ssd-back:0,import-from=/tmp/{{item.file}},format=qcow2
-qm set {{item.vmid}} --boot order=scsi0
+qm set {{item.vmid}} --virtio0 ssd-back:0,import-from=/tmp/{{item.file}},format=qcow2
+qm set {{item.vmid}} --boot order=virtio0
 qm set {{item.vmid}} --ide2  ssd-back:cloudinit
 qm set {{item.vmid}} --serial0 socket --vga serial0
 
