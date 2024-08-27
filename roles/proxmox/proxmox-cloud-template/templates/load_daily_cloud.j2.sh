abc9319e3e81af2dee06a67e37fb7995f715408d
CultureLinux <cyklodev.services@gmail.com>
2024-08-27 15:15:59 +0200

    param cloud image target_storage

--- roles/proxmox/proxmox-cloud-template/templates/load_daily_cloud.j2.sh
+++ roles/proxmox/proxmox-cloud-template/templates/load_daily_cloud.j2.sh
@@ -1,9 +1,9 @@
 qm destroy {{item.vmid}}
 
 qm create {{item.vmid}} --memory 1024 --core 1 --name {{item.vm}} --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci --description "{{item.vm}}"
-qm set {{item.vmid}} --virtio0 ssd-back:0,import-from=/tmp/{{item.file}},format=qcow2
+qm set {{item.vmid}} --virtio0 {{target_storage}}:0,import-from=/tmp/{{item.file}},format=qcow2
 qm set {{item.vmid}} --boot order=virtio0
-qm set {{item.vmid}} --ide2  ssd-back:cloudinit
+qm set {{item.vmid}} --ide2  {{target_storage}}:cloudinit
 qm set {{item.vmid}} --serial0 socket --vga serial0
 
 rm -fv /tmp/{{item.file}}
